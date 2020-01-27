e60961faf86c386dd538ae71350badaefd527f1d
edwardsioson86 <60221658+edwardsioson86@users.noreply.github.com>
2020-01-27 20:10:32 +0800

    Add files via upload

---
+++ LabActivity1_Sioson/LabActivity1_Sioson/Sample1_HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*18-03790
+Edward O. Sioson
+SD2A
+January 27, 2020
+This program will display the phrase "Hello World"*/
+
+namespace Sample1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LabActivity1_Sioson/Sample2_MyProfile/Sample2_MyProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2_MyProfile
+{
+    class Sample2_MyProfile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name \t\t:\t Edwar1d O. Sioson \n\n");
+            Console.WriteLine("Date of Birth \t:\t August 06, 1999 \n\n");
+            Console.WriteLine("Course \t\t:\t BS Computer Science Major in Software Development \n\n");
+            Console.WriteLine("Year \t\t:\t II \n\n");
+            Console.WriteLine("Section \t:\t SD2A \n\n");
+            Console.ReadKey();
+        }
+    }
+}
