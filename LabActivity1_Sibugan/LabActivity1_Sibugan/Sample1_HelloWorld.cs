515fc9d043d82a1d42d09134ce6962e2a0582d87
rovhijane <60381710+rovhijane@users.noreply.github.com>
2020-01-30 21:16:19 +0800

    Add files via upload

---
+++ LabActivity1_Sibugan/LabActivity1_Sibugan/Sample1_HelloWorld.cs
@@ -0,0 +1,24 @@
+﻿/*18-03115
+ * Rovhi Jane A. Sibugan
+ * BSCS-SD2A
+ * January 27, 2020
+ * This program will display the phrase "Hello World"*/
+ 
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1_Helloworld
+{
+    class Sample1_HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!!");
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ LabActivity1_Sibugan/Sample2_MyProfile/Sample2_MyProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2_MyProfile
+{
+    class Sample2_MyProfile
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Name\t\t:" + " \tRovhi Jane A. Sibugan\n\n");
+            System.Console.WriteLine("Date of Birth\t:" + " \tOctober 29, 1999\n\n");
+            System.Console.WriteLine("Course\t\t:" + " \tBSCS-Software Development\n\n");
+            System.Console.WriteLine("Year\t\t:" + " \t2nd Year\n\n");
+            System.Console.WriteLine("Section\t\t:" + " \tSD2A\n\n");
+            System.Console.ReadKey();
+
+
+        }
+    }
+}
