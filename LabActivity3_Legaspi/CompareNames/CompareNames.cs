b806e52177bf6be3be7c90aa96aea141eb710e7b
kenkennethneth <60118811+kenkennethneth@users.noreply.github.com>
2020-02-10 22:58:57 +0800

    Add files via upload

---
+++ LabActivity3_Legaspi/CompareNames/CompareNames.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareNames
+{
+    class CompareNames
+    {
+        static void Main(string[] args)
+        {
+            String string1 = "Kinit";
+            String string2 = "Kinit";
+            String string3 = "Kenneth";
+            String string4 = "Kenneth";
+            String string5 = "KENNETH";
+
+            Console.WriteLine("Using Equals() method");
+            Console.WriteLine("Compare {0} to {1}: {2}", string1, string2, String.Equals(string1, string2));
+            Console.WriteLine("Compare {0} to {1}: {2}", string1, string3, String.Equals(string1, string3));
+            Console.WriteLine("Length of {0} is {1}", string1, string1.Length);
+            Console.WriteLine("String {0} Substring(0, 3) will return {1}", string5, string5.Substring(0, 3));
+            Console.WriteLine("");
+
+            Console.WriteLine("Using Compare() method");
+            Console.WriteLine("Compare {0} to {1}: {2}", string1, string2, String.Compare(string1, string2));
+            Console.WriteLine("Compare {0} to {1}: {2}", string1, string3, String.Compare(string1, string3));
+            Console.WriteLine("Compare {0} to {1}: {2}", string3, string1, String.Compare(string3, string1));
+            Console.WriteLine("Compare {0} to {1}: {2}", string4, string5, String.Equals(string4, string5));
+            Console.WriteLine("");
+
+            Console.WriteLine("Using CompareTo() method");
+            Console.WriteLine("Compare {0} to {1}: {2}", string1, string2, string1.CompareTo(string2));
+            Console.WriteLine("Compare {0} to {1}: {2}", string1, string3, string1.CompareTo(string3));
+            Console.WriteLine("Compare {0} to {1}: {2}", string3, string1, string3.CompareTo(string1));
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabActivity3_Legaspi/GradingSystem/GradingSystem.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystem
+{
+    class GradingSystem
+    {
+        static void Main(string[] args)
+        {
+            string get;
+            string final_grade = null;
+            int final_grade1 = 0;
+
+            Console.Write("Enter your final grade: ");
+            get = Console.ReadLine();
+            
+            if(get.Equals("INC") || get.Equals("inc") || get.Equals("Inc") || get.Equals("INc") || get.Equals("InC"))
+            {
+                final_grade = get;
+            }
+            else
+            {
+                final_grade = " ";
+                final_grade1 = Convert.ToInt32(get);
+            }
+            
+            
+
+            if (final_grade.Equals("INC") || final_grade.Equals("inc") || final_grade.Equals("Inc"))
+            {
+                Console.WriteLine("Grade Equivalent: Incomplete");
+                Console.Write("Remarks: - ");
+            }
+            else if(final_grade1 >= 98 && final_grade1 <= 100)
+            {
+                Console.WriteLine("Grade Equivalent: 1.0");
+                Console.Write("Remarks: Excellent");
+            }
+            else if(final_grade1 >= 95 && final_grade1 <= 97)
+            {
+                Console.WriteLine("Grade Equivalent: 1.25");
+                Console.Write("Remarks: Very Good");
+            }
+            else if(final_grade1 >= 92 && final_grade1 <= 94)
+            {
+                Console.WriteLine("Grade Equivalent: 1.50");
+                Console.Write("Remarks: Very Good");
+            }
+            else if(final_grade1 >= 89 && final_grade1 <= 91)
+            {
+                Console.WriteLine("Grade Equivalent: 1.75");
+                Console.Write("Remarks: Very Good");
+            }
+            else if(final_grade1 >= 86 && final_grade1 <= 88)
+            {
+                Console.WriteLine("Grade Equivalent: 2.0");
+                Console.Write("Remarks: Good");
+            }
+            else if(final_grade1 >= 83 && final_grade1 <= 85)
+            {
+                Console.WriteLine("Grade Equivalent: 2.25");
+                Console.Write("Remarks: Good");
+            }
+            else if(final_grade1 >= 80 && final_grade1 <= 82)
+            {
+                Console.WriteLine("Grade Equivalent: 2.50");
+                Console.Write("Remarks: Fair");
+            }
+            else if(final_grade1 >= 77 && final_grade1 <= 79)
+            {
+                Console.WriteLine("Grade Equivalent: 2.75");
+                Console.Write("Remarks: Passed");
+            }
+            else if(final_grade1 >= 75 && final_grade1 <= 76)
+            {
+                Console.WriteLine("Grade Equivalent: 3.0");
+                Console.Write("Remarks: Passed");
+            }
+            else if(final_grade1 >= 72 && final_grade1 <= 74)
+            {
+                Console.WriteLine("Grade Equivalent: 4.0");
+                Console.Write("Remarks: Conditional");
+            }
+            else if(final_grade1 >= 60 && final_grade1 <= 71)
+            {
+                Console.WriteLine("Grade Equivalent: 5.0");
+                Console.Write("Remarks: Failed");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
